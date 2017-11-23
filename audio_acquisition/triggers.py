59d278b15a33e2dc4ad287d09deaf3daacec4079
Carl <carl.andersson@chalmers.se>
2017-11-23 14:59:34 +0100

    Unification of triggering
    Implements parts of a triggering framework in a Trigger class.
    Actual triggers should extend Trigger, and implement a 'test' method.

--- audio_acquisition/triggers.py
+++ audio_acquisition/triggers.py
@@ -1,17 +1,49 @@
-# import numpy as np
-# from threading import Event
+import numpy as np
+from threading import Event, Timer
 import logging
 
 from .utils import LevelDetector
 
 logger = logging.getLogger(__name__)
 
-class RMSTrigger:
-    def __init__(self, *, level, channel, action, fs, kind='Above', **kwargs):
+class Trigger:
+    def __init__(self, action=None, false_action=None):
+        self.active = Event()
+        self.active.set()
 
+        self.actions = []
+        if action is not None:
+            try:
+                self.actions.extend(action)
+            except TypeError:
+                self.actions.append(action)
+
+        self.false_actions = []
+        if false_action is not None:
+            try:
+                self.false_actions.extend(false_action)
+            except TypeError:
+                self.false_actions.append(false_action)
+
+    def __call__(self, block):
+        # We need to perform the test event if the triggering is disabled
+        # Some triggers (RMSTrigger) needs to update their state continiously to work as intended
+        # If e.g. RMSTrigger cannot update the level with the triggering disabled, it will always 
+        # start form zero
+        test = self.test(block)
+        if self.active.is_set():
+            # logger.debug('Testing in {}'.format(self.__class__.__name__))
+            if test:
+                logger.debug('Test conditions met in {}'.format(self.__class__.__name__))
+                [action() for action in self.actions]
+            else:
+                [action() for action in self.false_actions]
+
+class RMSTrigger(Trigger):
+    def __init__(self, *, level, channel, fs, action=None, region='Above', **kwargs):
+        Trigger.__init__(self, action=action)
         self.level_detector = LevelDetector(channel=channel, fs=fs, **kwargs)
-        self.action = action
-        self.kind = kind
+        self.region = region
         self.trigger_level = level
 
     # TODO: If the loop is not fast enough there are a few options.
@@ -20,29 +52,37 @@ class RMSTrigger:
     # It should also be possible (I think) to write the level detector using Faust, and just drop in the
     # wrapped Faust code here. If I manage to get that working it could also be used for all other crazy
     # filters we might want to use.
-    def __call__(self, block):
-        # trigger_on = self._event.is_set()
-        logger.debug('RMSTrigger called!')
+    def test(self, block):
+        # logger.debug('Testing in RMS trigger')
         levels = self.level_detector(block)
+        return any(self._sign * levels > self.trigger_level * self._sign)
 
-        # This will switch the state if the trigger level is passed at least once
-        # It should be more robust for transients: If there is a transient that turns on the triggering
-        # we do not care if the level dropped afterwards.
-        if any(self._kind_sign * levels > self.trigger_level * self._kind_sign):
-            self.action()
+    # def __call__(self, block):
+    #     # trigger_on = self._event.is_set()
+    #     # logger.debug('RMSTrigger called!')
+    #     levels = self.level_detector(block)
+
+    #     # This will switch the state if the trigger level is passed at least once
+    #     # It should be more robust for transients: If there is a transient that turns on the triggering
+    #     # we do not care if the level dropped afterwards.
+    #     if any(self._kind_sign * levels > self.trigger_level * self._kind_sign):
+    #         logger.debug('Trigger active')
+    #         [action() for action in self.active_actions]
+    #     else:
+    #         [action() for action in self.deactive_actions]
 
     @property
-    def kind(self):
-        if self._kind_sign == 1:
+    def region(self):
+        if self._sign == 1:
             return 'Above'
         else:
             return 'Below'
 
-    @kind.setter
-    def kind(self, value):
+    @region.setter
+    def region(self, value):
         if value.lower() == 'above':
-            self._kind_sign = 1
+            self._sign = 1
         elif value.lower() == 'below':
-            self._kind_sign = -1
+            self._sign = -1
         else:
-            raise ValueError('Kind {} not a valid kind of RMS trigger.'.format(value))
+            raise ValueError('{} not a valid regoin for RMS trigger.'.format(value))
