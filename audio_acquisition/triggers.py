a84af29d0b6e03b531c1a1ba5972e6565e8ee952
Carl <carl.andersson@chalmers.se>
2017-11-23 15:00:17 +0100

    Implementation of more triggers
    A PeakTrigger is implemented, as well as a class for delaying actions.

--- audio_acquisition/triggers.py
+++ audio_acquisition/triggers.py
@@ -86,3 +86,48 @@ class RMSTrigger(Trigger):
             self._sign = -1
         else:
             raise ValueError('{} not a valid regoin for RMS trigger.'.format(value))
+
+class PeakTrigger(Trigger):
+    def __init__(self, *, level, channel, action, region='Above'):
+        Trigger.__init__(self, action=action)
+        # self.action = action
+        self.region = region
+        self.trigger_level = level
+        self.channel = channel
+
+    def test(self, block):
+        # logger.debug('Testing in Peak triggger')
+        levels = np.abs(block[self.channel])
+        return any(self._sign * levels > self.trigger_level * self._sign)
+
+    # def __call__(self, block):
+    #     levels = np.abs(block[channel])
+    #     if any(self._sign * levels > self.trigger_level * self._sign):
+    #         self.action()
+
+    @property
+    def region(self):
+        if self._sign == 1:
+            return 'Above'
+        else:
+            return 'Below'
+
+    @region.setter
+    def region(self, value):
+        if value.lower() == 'above':
+            self._sign = 1
+        elif value.lower() == 'below':
+            self._sign = -1
+        else:
+            raise ValueError('{} not a valid region for peak trigger.'.format(value))
+
+class DelayedAction:
+    def __init__(self, *, action, time):
+        self.action = action
+        self.time = time
+        # self.timer = Timer(interval=time, function=action)
+
+    def __call__(self):
+        timer = Timer(interval=self.time, function=self.action)
+        timer.start()
+        # self.timer.start()
