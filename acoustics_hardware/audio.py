e8f60f32a31003f11f351f6b977f5df371d8004b
Carl Andersson <carl.andersson@chalmers.se>
2018-02-14 16:47:38 +0100

    Implements AudioDevice
    A working implementation for audio devices using `sounddevice`.
    
    This is not stress-tested!

--- acoustics_hardware/audio.py
+++ acoustics_hardware/audio.py
@@ -1,3 +1,4 @@
+import queue
 import sounddevice as sd
 from . import core
 
@@ -6,11 +7,12 @@ def get_devices(name=None):
     if name is None:
         return sd.query_devices()
     else:
-        return sd.query_devices(name)[name]
+        return sd.query_devices(name)['name']
 
 
 class AudioDevice(core.Device):
     def __init__(self, name=None, fs=None, framesize=None):
+        core.Device.__init__(self)
         if name is None:
             self.name = get_devices()[0]['name']
         else:
@@ -32,17 +34,44 @@ class AudioDevice(core.Device):
         self.outputs = []
 
     def _hardware_run(self):
-        raise NotImplementedError
-
         # Create streams
-        if len(self.inputs) and len(self.outputs):
+        num_output_ch = max(self.outputs, default=-1) + 1
+        num_input_ch = max(self.inputs, default=-1) + 1
+        self.silent_ch = [ch for ch in range(num_output_ch) if ch not in self.outputs]
+        if num_input_ch and num_output_ch:
             # in/out stream
-        elif len(self.inputs):
-            # input stream
+            def callback(indata, outdata, frames, time, status):
+                self._input_callback(indata)
+                self._output_callback(outdata)
+            stream = sd.Stream(device=self.name, samplerate=self.fs, blocksize=self.framesize, callback=callback, channels=(num_input_ch, num_output_ch))
+        elif num_input_ch:
+            def callback(indata, frames, time, status):
+                self._input_callback(indata)
+            stream = sd.InputStream(device=self.name, samplerate=self.fs, blocksize=self.framesize, callback=callback, channels=num_input_ch)
         elif len(self.outputs):
-            # output stream
+            def callback(outdata, frames, time, status):
+                self._output_callback(outdata)
+            stream = sd.OutputStream(device=self.name, samplerate=self.fs, blocksize=self.framesize, callback=callback, channels=num_output_ch)
         else:
             # Do something??
+            pass
+
+        stream.start()
+        self._hardware_stop_event.wait()
+        stream.stop()
+
+    def _input_callback(self, indata):
+        self._hardware_input_Q.put(indata.T[self.inputs].copy())  # Transpose before copy to actually reverse the memory layout
+
+    def _output_callback(self, outdata):
+        if self.output_active.is_set():
+            try:
+                outdata[:, self.outputs] = self._hardware_output_Q.get_nowait().T
+                outdata[:, self.silent_ch] = 0
+            except queue.Empty:
+                outdata[:] = 0
+        else:
+            outdata[:] = 0
 
     def add_input(self, idx):
         if idx not in self.inputs and idx < self.max_inputs:
