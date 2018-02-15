b3d3061bb0ac28ce71ef806e59f592e0c23e433a
Carl Andersson <carl.andersson@chalmers.se>
2018-02-15 16:42:23 +0100

    Implements signal generator framework
    Framework for easy implementation of different signal generators, e.g.
    sinusoidal tones, sweeps, MLS.
    
    Subclasses need to implement `setup` to create the actual signal.

--- acoustics_hardware/generators.py
+++ acoustics_hardware/generators.py
@@ -30,3 +30,43 @@ class QGenerator(core.Generator):
 
     def reset(self):
         utils.flush_Q(self.Q)
+
+
+class SignalGenerator(core.Generator):
+    def __init__(self):
+        core.Generator.__init__(self)
+        self.signal = None
+        self.repetitions = np.inf  # Default to continious output
+        self.reset()
+
+    def __call__(self):
+        if self.repetitions_done >= self.repetitions:
+            raise core.GeneratorStop('Finite number of repetitions reached')
+        if self.signal is None:
+            self.setup()
+        samples_left = self._device.framesize
+        gen_frame = []
+        while samples_left > 0:
+            gen_frame.append(self.signal[..., self.idx:self.idx + samples_left])
+            samps_app = gen_frame[-1].shape[-1]
+            samples_left -= samps_app
+            self.idx += samps_app
+            if self.idx >= self.signal.shape[-1]:
+                self.repetitions_done += 1
+                self.idx = 0
+                if self.repetitions_done >= self.repetitions:
+                    gen_frame.append(np.zeros(self.signal.shape[:-1] + (samples_left,)))
+                    samples_left = 0
+        return np.concatenate(gen_frame, axis=-1)
+
+    def reset(self):
+        self.idx = 0
+        self.repetitions_done = 0
+
+    def setup(self):
+        """
+        Implements signal creation. Create one cycle of the signal and store it
+        in `self.signal`. Access the underlying device as `self._device`, which has
+        important properties, e.g. samplerate `fs`.
+        """
+        raise NotImplementedError('Required method `setup` not implemented in {}'.format(self.__class__.__name__))
