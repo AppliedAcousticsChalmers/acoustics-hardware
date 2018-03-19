5a9637c8c2630237d06c51b73f8bfd9b86877c15
Carl Andersson <carl.andersson@chalmers.se>
2018-03-19 14:50:02 +0100

    Update setup
    The optional extras are no longer optional, so all modules are required.
    Automatically gets the version number from the package `__init__`.
    
    Includes github repo, author, and authon email in the descriptions.

--- acoustics_hardware/__init__.py
+++ acoustics_hardware/__init__.py
@@ -1,6 +1,6 @@
 import logging
 
-
+__version__ = '0.0.1.dev1'
 logger = logging.getLogger(__name__)
 
 __all__ = ['core', 'triggers', 'io', 'utils']
--- setup.py
+++ setup.py
@@ -1,22 +1,27 @@
 from setuptools import setup
 
-# TODO: Add github url
-# TODO: Add author
-# TODO; Add author email
-# TODO: Add licence
+__version__ = 'unknown'
+for line in open('acoustics_hardware/__init__.py'):
+    if line.startswith('__version__'):
+        exec(line)
+        break
 
 
-setup(
-    name='acoustics-hardware',
-    version='0.0.1',
-    description='Controlling hardware used in acoustic measurement systems',
-    packages=['acoustics_hardware'],
-    install_requires=[
-        'numpy',
-        'scipy'],
-    extras_require={
-        'hdf': ['h5py'],
-        'ni': ['nidaqmx;platform_system=="Windows"'],
-        'serial': ['pyserial'],
-        'motors': ['SchunkMotionProtocol']
-    })
+setup(name='acoustics-hardware',
+      version=__version__,
+      description='Controlling hardware used in acoustic measurement systems',
+      long_description=open('README.RST').read(),
+      url='https://github.com/CarlAndersson/acoustics-hardware',
+      author='Carl Andersson',
+      author_email='carl.andersson@chalmers.se',
+      license='MIT',
+      packages=['acoustics_hardware'],
+      install_requires=[
+          'numpy',
+          'scipy',
+          'h5py',
+          'sounddevice',
+          'nidaqmx;platform_system=="Windows"',
+          'pyserial',
+          'SchunkMotionProtocol'],
+      )
