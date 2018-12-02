816d5de897a4e25d5396a87cd1e3f1e84006c6a7
Carl <carl.andersson@chalmers.se>
2018-12-02 20:20:51 +0100

    Enables multiple modules in single NIDevice
    Instead of representing a module with a device, we can represent a chassis with a device.
    This allows for multiple modules to be used as a single multichannel device when
    using a multi-slot chassis. If inputs and outputs are used simultaneously, the modules
    will be syncronized using shared timebases. This is tested only using NI-9234 and NI-9260.

--- acoustics_hardware/devices.py
+++ acoustics_hardware/devices.py
@@ -126,8 +126,8 @@ class NIDevice(core.Device):
         The framesize needs reasonable defaults, and needs to be protected
         by validity checks.
     """
-    @staticmethod
-    def get_devices(name=None):
+    @classmethod
+    def get_devices(cls, name=None):
         """Check which NI hardware is available.
 
         Since `NIDevice` is intended to interact with a single module at the
@@ -152,23 +152,44 @@ class NIDevice(core.Device):
         else:
             if len(name) == 0:
                 name = name_list[0]
-            if name[:4] == 'cDAQ' and name[5:8] != 'Mod':
-                name = [x for x in name_list if x[:8] == name[:5] + 'Mod'][0]
             return name
+
+    @classmethod
+    def get_chassis(cls, name=None):
+        devs = cls.get_devices()
+        chassis = [dev for dev in devs if 'cDAQ' in dev and 'Mod' not in dev]
+        if name is None:
+            return chassis
+        if name in chassis:
+            return name
+        return [dev for dev in chassis if str(name) in dev][0]
+
+    @classmethod
+    def get_modules(cls, chassis):
+        chassis = cls.get_chassis(chassis)
+        devs = cls.get_devices()
+        return [dev for dev in devs if chassis in dev and 'Mod' in dev]
+
+    @classmethod
+    def reset_chassis_modules(cls, chassis):
+        for module in cls.get_modules(chassis):
+            nidaqmx.system.Device(module).reset_device()
 
     def __init__(self, name=None, fs=None, framesize=10000, dtype='float64', **kwargs):
         super().__init__(**kwargs)
         if name is None:
             name = NIDevice.get_devices()[0]
-        self.name = NIDevice.get_devices(name)
+        self.chassis = self.get_chassis(name)
+        self.modules = self.get_modules(self.chassis)
+        self.input_modules = [module for module in self.modules if len(nidaqmx.system.Device(module).ai_physical_chans) > 0]
+        self.output_modules = [module for module in self.modules if len(nidaqmx.system.Device(module).ao_physical_chans) > 0]
         if fs is None:
-            try:
-                self.fs = nidaqmx.system.Device(self.name).ai_max_single_chan_rate
-            except nidaqmx.DaqError as e:
-                if e.error_type == nidaqmx.error_codes.DAQmxErrors.ATTR_NOT_SUPPORTED:
-                    self.fs = nidaqmx.system.Device(self.name).ao_max_rate
-                else:
-                    raise
+            if len(self.input_modules) > 0:
+                self.fs = nidaqmx.system.Device(self.input_modules[0]).ai_max_single_chan_rate
+            elif len(self.output_modules) > 0:
+                self.fs = nidaqmx.system.Device(self.output_modules[0]).ao_max_rate
+            else:
+                raise AttributeError('Device has no modules!')
         else:
             self.fs = fs
         self.framesize = framesize
@@ -176,31 +197,63 @@ class NIDevice(core.Device):
 
     @property
     def max_inputs(self):
-        return len(nidaqmx.system.Device(self.name).ai_physical_chans)
+        inputs = 0
+        for module in self.input_modules:
+            inputs += len(nidaqmx.system.Device(module).ai_physical_chans)
+        return inputs
 
     @property
     def max_outputs(self):
-        return len(nidaqmx.system.Device(self.name).ao_physical_chans)
+        outputs = 0
+        for module in self.output_modules:
+            outputs += len(nidaqmx.system.Device(module).ao_physical_chans)
+        return outputs
 
-    @property
-    def input_range(self):
-        """Returns the input range on the device.
+    def input_map(self, index=None, module=None, channel=None):
+        if index is not None:
+            # Mapping from index to module and channel
+            module = 0
+            channel = index
+            while channel >= len(nidaqmx.system.Device(self.input_modules[module]).ai_physical_chans):
+                channel -= len(nidaqmx.system.Device(self.input_modules[module]).ai_physical_chans)
+                module += 1
+            return module, channel
+        elif channel is not None and module is not None:
+            # Mapping from channel and module to index
+            index = 0
+            for mod in range(module-1):
+                index += len(nidaqmx.system.Device(self.input_modules[mod]).ai_physical_chans)
+            index += channel
+            return index
+        else:
+            mapping = []
+            for input in self.inputs:
+                module, channel = self.input_map(index=input)
+                mapping.append(nidaqmx.system.Device(self.input_modules[module]).ai_physical_chans[channel].name)
+            return mapping
 
-        Note:
-            This is only an approximate value, do NOT use for calibrating
-            unscaled readings.
-        """
-        return nidaqmx.system.Device(self.name).ai_voltage_rngs
-
-    @property
-    def output_range(self):
-        """Returns the output range on the device.
-
-        Note:
-            This is only an approximate value, do NOT use for calibrating
-            unscaled outputs.
-        """
-        return nidaqmx.system.Device(self.name).ao_voltage_rngs
+    def output_map(self, index=None, module=None, channel=None):
+        if index is not None:
+            # Mapping from index to module and channel
+            module = 0
+            channel = index
+            while channel >= len(nidaqmx.system.Device(self.output_modules[module]).ao_physical_chans):
+                channel -= len(nidaqmx.system.Device(self.output_modules[module]).ao_physical_chans)
+                module += 1
+            return module, channel
+        elif channel is not None and module is not None:
+            # Mapping from channel and module to index
+            index = 0
+            for mod in range(module-1):
+                index += len(nidaqmx.system.Device(self.output_modules[mod]).ao_physical_chans)
+            index += channel
+            return index
+        else:
+            mapping = []
+            for output in self.outputs:
+                module, channel = self.output_map(index=output)
+                mapping.append(nidaqmx.system.Device(self.output_modules[module]).ao_physical_chans[channel].name)
+            return mapping
 
     def bit_depth(self, channel=None):
         """The bitdepth for the device.
@@ -215,7 +268,7 @@ class NIDevice(core.Device):
             ch_idx = 0
         else:
             ch_idx = self.inputs.index(channel)
-        return self._task.ai_channels[ch_idx].ai_resolution
+        return self._input_task.ai_channels[ch_idx].ai_resolution
 
     def word_length(self, channel=None):
         """The word length for the device.
@@ -227,10 +280,10 @@ class NIDevice(core.Device):
             - Chack that the task exists, else warn.
         """
         if channel is None:
-            return max([ch.ai_raw_samp_size for ch in self._task.ai_channels])
+            return max([ch.ai_raw_samp_size for ch in self._input_task.ai_channels])
         else:
             ch_idx = self.inputs.index(channel)
-            return self._task.ai_channels[ch_idx].ai_raw_samp_size
+            return self._input_task.ai_channels[ch_idx].ai_raw_samp_size
 
     def scaling_coeffs(self, channels=None):
         """Scaling coefficients used while reading raw input.
@@ -247,92 +300,100 @@ class NIDevice(core.Device):
             ch_idx = [self.inputs.index(ch) for ch in channels]
         except TypeError:
             ch_idx = self.inputs.index(channels)
-            return self._task.ai_channels[ch_idx].ai_dev_scaling_coeff
+            return self._input_task.ai_channels[ch_idx].ai_dev_scaling_coeff
         else:
-            return [self._task.ai_channels[idx].ai_dev_scaling_coeff for idx in ch_idx]
+            return [self._input_task.ai_channels[idx].ai_dev_scaling_coeff for idx in ch_idx]
 
     def _hardware_run(self):
-        """
-        Todo:
-            - Merge the callback creation function internally in this function.
-                Since they will never be used outside of here, and only once,
-                it does not make much sense to keep them as functions.
-            - Handle disallowed sample rates
-        """
-        nidaqmx.system.Device(self.name).reset_device()
-        self._task = nidaqmx.Task()
+        self.reset_chassis_modules(self.chassis)
+        self._input_task = nidaqmx.Task()
+        self._output_task = nidaqmx.Task()
+
         for ch in self.inputs:
-            self._task.ai_channels.add_ai_voltage_chan(self.name + '/ai{}'.format(int(ch)))
+            module, channel = self.input_map(index=ch)
+            self._input_task.ai_channels.add_ai_voltage_chan(self.input_modules[module] + '/ai{}'.format(int(channel)))
         for ch in self.outputs:
-            self._task.ao_channels.add_ao_voltage_chan(self.name + '/ao{}'.format(int(ch)), min_val=self.output_range[0], max_val=self.output_range[1])
-        if len(self.inputs) or len(self.outputs):
-            self._task.timing.cfg_samp_clk_timing(int(self.fs), samps_per_chan=self.framesize,
+            module, channel = self.output_map(index=ch)
+            output_range = nidaqmx.system.Device(self.output_modules[module]).ao_voltage_rngs
+            self._output_task.ao_channels.add_ao_voltage_chan(self.output_modules[module] + '/ao{}'.format(int(channel)), min_val=output_range[0], max_val=output_range[1])
+        if len(self.inputs) and len(self.outputs):
+            self._input_task.timing.cfg_samp_clk_timing(int(self.fs), samps_per_chan=self.framesize,
+                    sample_mode=nidaqmx.constants.AcquisitionType.CONTINUOUS)
+            self._output_task.timing.cfg_samp_clk_timing(int(self.fs), samps_per_chan=self.framesize,
+                    sample_mode=nidaqmx.constants.AcquisitionType.CONTINUOUS)
+            self._output_task.timing.samp_clk_timebase_src = self._input_task.timing.samp_clk_timebase_src
+            self._output_task.timing.samp_clk_timebase_rate = self._input_task.timing.samp_clk_timebase_rate
+        elif len(self.inputs):
+            self._input_task.timing.cfg_samp_clk_timing(int(self.fs), samps_per_chan=self.framesize,
+                    sample_mode=nidaqmx.constants.AcquisitionType.CONTINUOUS)
+        elif len(self.outputs):
+            self._output_task.timing.cfg_samp_clk_timing(int(self.fs), samps_per_chan=self.framesize,
                     sample_mode=nidaqmx.constants.AcquisitionType.CONTINUOUS)
 
         if self.dtype.lower() == 'unscaled' or self.dtype.lower() == 'int':
             wl = self.word_length()
             self.dtype = 'int{}'.format(int(wl))
-        if len(self.inputs) > 0:
-            self._task.register_every_n_samples_acquired_into_buffer_event(self.framesize, self._create_input_callback())
+
+        if len(self.inputs):
+            if self.dtype == 'int16':
+                reader = nidaqmx.stream_readers.AnalogUnscaledReader(self._input_task.in_stream)
+                databuffer = np.empty((len(self.inputs), self.framesize), dtype='int16')
+                read_function = reader.read_int16
+            elif self.dtype == 'int32':
+                reader = nidaqmx.stream_readers.AnalogUnscaledReader(self._input_task.in_stream)
+                databuffer = np.empty((len(self.inputs), self.framesize), dtype='int32')
+                read_function = reader.read_int32
+            elif self.dtype == 'uint16':
+                reader = nidaqmx.stream_readers.AnalogUnscaledReader(self._input_task.in_stream)
+                databuffer = np.empty((len(self.inputs), self.framesize), dtype='uint16')
+                read_function = reader.read_uint16
+            elif self.dtype == 'uint32':
+                reader = nidaqmx.stream_readers.AnalogUnscaledReader(self._input_task.in_stream)
+                databuffer = np.empty((len(self.inputs), self.framesize), dtype='uint32')
+                read_function = reader.read_uint32
+            else:  # Read as scaled float64
+                reader = nidaqmx.stream_readers.AnalogMultiChannelReader(self._input_task.in_stream)
+                databuffer = np.empty((len(self.inputs), self.framesize), dtype='float64')
+                read_function = reader.read_many_sample
+
+            def input_callback(task_handle, every_n_samples_event_type,
+                               number_of_samples, callback_data):
+                sampsRead = read_function(databuffer, self.framesize)
+                self._hardware_input_Q.put(databuffer.copy())
+                return 0
+            self._input_task.register_every_n_samples_acquired_into_buffer_event(self.framesize, input_callback)
+            self._input_task.start()
+
         if len(self.outputs):
-                self._task.register_every_n_samples_transferred_from_buffer_event(self.framesize, self._create_output_callback())
+            writer = nidaqmx.stream_writers.AnalogMultiChannelWriter(self._output_task.out_stream)
+            write_funciton = writer.write_many_sample
+            self._output_task.out_stream.regen_mode = nidaqmx.constants.RegenerationMode.DONT_ALLOW_REGENERATION  # Needed to prevent issues with buffer overwrites and reuse
+            write_funciton(np.zeros((self._output_task.out_stream.num_chans, 2*self.framesize)))  # Pre-fill the buffer with zeros, there needs to be something in the buffer when we start
+            timeout = 0.95 * self.framesize / self.fs
 
-        self._task.start()
+            def output_callback(task_handle, every_n_samples_event_type,
+                                number_of_samples, callback_data):
+                try:
+                    data = self._hardware_output_Q.get(timeout=timeout)
+                except queue.Empty:
+                    data = np.zeros((self._output_task.out_stream.num_chans, number_of_samples))
+                else:
+                    self._hardware_output_Q.task_done()
+                finally:
+                    sampsWritten = write_funciton(data)
+                return 0
+            self._output_task.register_every_n_samples_transferred_from_buffer_event(self.framesize, output_callback)
+            self._output_task.start()
+
         self._hardware_stop_event.wait()
-        self._task.stop()
-        self._task.wait_until_done(timeout=10)
-        self._task.close()
-        del self._task
-        nidaqmx.system.Device(self.name).reset_device()
 
-    def _create_input_callback(self):
-        if self.dtype == 'int16':
-            reader = nidaqmx.stream_readers.AnalogUnscaledReader(self._task.in_stream)
-            databuffer = np.empty((len(self.inputs), self.framesize), dtype='int16')
-            read_function = reader.read_int16
-        elif self.dtype == 'int32':
-            reader = nidaqmx.stream_readers.AnalogUnscaledReader(self._task.in_stream)
-            databuffer = np.empty((len(self.inputs), self.framesize), dtype='int32')
-            read_function = reader.read_int32
-        elif self.dtype == 'uint16':
-            reader = nidaqmx.stream_readers.AnalogUnscaledReader(self._task.in_stream)
-            databuffer = np.empty((len(self.inputs), self.framesize), dtype='uint16')
-            read_function = reader.read_uint16
-        elif self.dtype == 'uint32':
-            reader = nidaqmx.stream_readers.AnalogUnscaledReader(self._task.in_stream)
-            databuffer = np.empty((len(self.inputs), self.framesize), dtype='uint32')
-            read_function = reader.read_uint32
-        else:  # Read as scaled float64
-            reader = nidaqmx.stream_readers.AnalogMultiChannelReader(self._task.in_stream)
-            databuffer = np.empty((len(self.inputs), self.framesize), dtype='float64')
-            read_function = reader.read_many_sample
-
-        def input_callback(task_handle, every_n_samples_event_type,
-                           number_of_samples, callback_data):
-            sampsRead = read_function(databuffer, self.framesize)
-            self._hardware_input_Q.put(databuffer.copy())
-            return 0
-        return input_callback
-
-    def _create_output_callback(self):
-        writer = nidaqmx.stream_writers.AnalogMultiChannelWriter(self._task.out_stream)
-        write_funciton = writer.write_many_sample
-        self._task.out_stream.regen_mode = nidaqmx.constants.RegenerationMode.DONT_ALLOW_REGENERATION  # Needed to prevent issues with buffer overwrites and reuse
-        write_funciton(np.zeros((self._task.out_stream.num_chans, 2*self.framesize)))  # Pre-fill the buffer with zeros, there needs to be something in the buffer when we start
-        timeout = 0.95 * self.framesize / self.fs
-
-        def output_callback(task_handle, every_n_samples_event_type,
-                            number_of_samples, callback_data):
-            try:
-                data = self._hardware_output_Q.get(timeout=timeout)
-            except queue.Empty:
-                data = np.zeros((self._task.out_stream.num_chans, number_of_samples))
-            else:
-                self._hardware_output_Q.task_done()
-            finally:
-                sampsWritten = write_funciton(data)
-            return 0
-        return output_callback
+        self._input_task.stop()
+        self._output_task.stop()
+        self._input_task.wait_until_done(timeout=10)
+        self._output_task.wait_until_done(timeout=10)
+        self._input_task.close()
+        self._output_task.close()
+        self.reset_chassis_modules(self.chassis)
 
 
 class FeedbackDevice(core.Device):
