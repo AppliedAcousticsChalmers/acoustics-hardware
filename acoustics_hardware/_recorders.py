9c8e74bbae839a0555d262f095b34d3dee5437c3
Carl Andersson <carl.andersson@ivl.se>
2022-04-12 17:05:33 +0200

    Implements basic recorder
    The most basic recorder which just stores all the frames in a list.

---
+++ acoustics_hardware/_recorders.py
@@ -0,0 +1,24 @@
+from . import _core
+import numpy as np
+
+
+class _Recorder(_core.SamplerateFollower):
+    ...
+
+
+class InternalRecorder(_Recorder):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.reset()
+
+    def reset(self, **kwargs):
+        super().reset(**kwargs)
+        self._storage = []
+
+    def process(self, frame):
+        self._storage.append(frame)
+        return frame
+
+    @property
+    def recorded_data(self):
+        return np.concatenate(self._storage, axis=-1)
