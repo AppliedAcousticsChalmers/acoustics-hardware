48330fe8643a5f9954a0f591a81026e968cf8b2c
Carl Andersson <carl.andersson@ivl.se>
2022-05-25 09:13:54 +0200

    Adds sweep and mls signals
    These are two commonly used measurement signals.

--- acoustics_hardware/_generators.py
+++ acoustics_hardware/_generators.py
@@ -1,5 +1,6 @@
 from . import _core, signal_tools
 import numpy as np
+import scipy.signal
 
 
 class _Generator(_core.SamplerateFollower):
@@ -67,4 +68,39 @@ class SignalGenerator(_Generator):
                     raise _core.PipelineStop()
                 frame = signal_tools.extend_signals(first_part, length=framesize)
         return super().process(frame)
+
+
+class SweepGenerator(SignalGenerator):
+    def __init__(
+        self,
+        lower_frequency, upper_frequency,
+        sweep_length, method='logarithmic',
+        **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.lower_frequency = lower_frequency
+        self.upper_frequency = upper_frequency
+        self.sweep_length = sweep_length
+        self.method = method
+
+    def setup(self, **kwargs):
+        super().setup(**kwargs)
+        time_vector = np.arange(round(self.sweep_length * self.samplerate)) / self.samplerate
+        signal = scipy.signal.chirp(t=time_vector, f0=self.lower_frequency, t1=self.sweep_length, f1=self.upper_frequency, method=self.method, phi=-90)
+        last_crossing = -1
+        while np.sign(signal[last_crossing - 1]) == np.sign(signal[-1]):
+            last_crossing -= 1
+        signal[last_crossing:] = 0
+        self.signal = signal
+
+
+class MaximumLengthGenerator(SignalGenerator):
+    def __init__(self, order, **kwargs):
+        super().__init__(**kwargs)
+        self.order = order
+
+    def setup(self, **kwargs):
+        super().setup(**kwargs)
+        self.sequence, _ = scipy.signal.max_len_seq(self.order)
+        self.signal = (1 - 2 * self.sequence).astype('float64')
 
