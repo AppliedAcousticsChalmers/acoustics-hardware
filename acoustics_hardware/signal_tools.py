5e4f94b1ef9c96d09293ee20b9253c3720b56ae1
Carl Andersson <carl.andersson@ivl.se>
2022-06-23 12:30:36 +0200

    Adds more flexible length specifications
    This allows specifying fade and pad lengths as fractions or percentages
    as well as samples or time.

--- acoustics_hardware/signal_tools.py
+++ acoustics_hardware/signal_tools.py
@@ -1,10 +1,30 @@
 import numpy as np
+import fractions
 
 
 def _apply_to_signals(func, signals):
     if isinstance(signals, np.ndarray):
         return func(signals)
     return tuple(func(signal) for signal in signals)
+
+
+def _length_parser(length, samplerate):
+    if isinstance(length, str):
+        if '%' in length:
+            length = float(length.strip('%')) / 100
+            def parser(signal):
+                return round(signal.shape[-1] * length)
+        else:
+            length = fractions.Fraction(length)
+            def parser(signal):
+                return round(signal.shape[-1] * length)
+    else:
+        length = length or 0
+        if samplerate is not None:
+            length = round(samplerate * length)
+        def parser(signal):
+            return length
+    return parser
 
 
 def truncate_signals(signals, length=None, samplerate=None):
@@ -46,48 +66,40 @@ def pad_signals(signals, pre_pad=None, post_pad=None, samplerate=None):
     if pre_pad is None and post_pad is None:
         return signals
 
-    if samplerate is not None:
-        if pre_pad is not None:
-            pre_pad = round(samplerate * pre_pad)
-        if post_pad is not None:
-            post_pad = round(samplerate * post_pad)
-    pre_pad = pre_pad or 0
-    post_pad = post_pad or 0
-    pre_pad = np.zeros(signals.shape[:-1] + (pre_pad,))
-    post_pad = np.zeros(signals.shape[:-1] + (post_pad,))
-
+    pre_pad = _length_parser(pre_pad, samplerate)
+    post_pad = _length_parser(post_pad, samplerate)
     def pad(signal):
-        return np.concatenate([pre_pad, signal, post_pad], axis=-1)
+        pre = np.zeros(signal.shape[:-1] + (pre_pad(signal),))
+        post = np.zeros(signal.shape[:-1] + (post_pad(signal),))
+        return np.concatenate([pre, signal, post], axis=-1)
 
     return _apply_to_signals(pad, signals)
 
 
-def fade_signals(signals, fade_in=None, fade_out=None, samplerate=None, inplace=True):
+def fade_signals(signals, fade_in=None, fade_out=None, samplerate=None, inplace=False):
     if fade_in is None and fade_out is None:
         return signals
 
-    if samplerate is not None:
-        if fade_in is not None:
-            fade_in = round(samplerate * fade_in)
-        if fade_out is not None:
-            fade_out = round(samplerate * fade_out)
-
-    if fade_in is None:
-        fade_in = np.ones(shape=0)
-    else:
-        fade_in = np.sin(np.linspace(0, np.pi / 2, fade_in))**2
-    if fade_out is None:
-        fade_out = np.ones(shape=0)
-    else:
-        fade_out = np.sin(np.linspace(np.pi / 2, 0, fade_out))**2
+    fade_in = _length_parser(fade_in, samplerate)
+    fade_out = _length_parser(fade_out, samplerate)
 
     def fade(signal):
-        # Apply the fade in and the fade out
-        # Make sure to respect the `inplace` variable!
         if not inplace:
             signal = signal.copy()
-        signal[..., :fade_in.size] *= fade_in
-        signal[..., -fade_out.size:] *= fade_out
+        in_samples = fade_in(signal)
+        out_samples = fade_out(signal)
+        signal[..., :in_samples] *= np.sin(np.linspace(0, np.pi / 2, in_samples))**2
+        signal[..., -out_samples:] *= np.sin(np.linspace(np.pi / 2, 0, out_samples))**2
         return signal
 
     return _apply_to_signals(fade, signals)
+
+
+def nonzero_signals(signals, inplace=False):
+    def nonzero(signal):
+        if not inplace:
+            signal = signal.copy()
+        zero_indices = signal == 0
+        signal[zero_indices] = np.min(np.abs(signal[np.invert(zero_indices)]))
+        return signal
+    return _apply_to_signals(nonzero, signals)
