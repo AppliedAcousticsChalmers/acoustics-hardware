aae6e03a9c3980e7dc4d1bbdf562541ecc51e9d0
Carl Andersson <carl.andersson@ivl.se>
2022-06-23 13:09:32 +0200

    Improves sweep deconvolution
    There was some omissions in the sweep deconvolution that caused the
    different methods to yield results with different time references.
    This patches the differences and changes the implementation such that
    the inversion methods should do the actual inversion, not just make an
    inverse filter.

--- acoustics_hardware/analysis.py
+++ acoustics_hardware/analysis.py
@@ -19,60 +19,33 @@ def sweep_deconvolution(
 ):
     if isinstance(inversion_method, str):
         if inversion_method.lower() == 'filtering':
-            inverse_filter = _filtered_inverse(
-                reference_signal=reference_signal,
-                samplerate=samplerate,
-                lower_cutoff=lower_cutoff,
-                upper_cutoff=upper_cutoff,
-                **kwargs
-            )
+            inversion_method = _filtered_deconvolution
         elif inversion_method.lower() == 'regularized':
-            inverse_filter = _regularized_inverse(
-                reference_signal=reference_signal,
-                samplerate=samplerate,
-                lower_cutoff=lower_cutoff,
-                upper_cutoff=upper_cutoff,
-                **kwargs
-            )
+            inversion_method = _regularized_deconvolution
         elif inversion_method.lower() == 'analytic':
-            inverse_filter = _exponential_sweep_analytical_inverse(
-                num_samples=reference_signal.size,
-                samplerate=samplerate,
-                lower_cutoff=lower_cutoff,
-                upper_cutoff=upper_cutoff,
-                **kwargs
-            )
+            inversion_method = _exponential_sweep_analytical_deconvolution
         elif inversion_method.lower() == 'time-reversal':
-            inverse_filter = _exponential_sweep_time_reversal_inverse(
-                reference_signal=reference_signal,
-                lower_cutoff=lower_cutoff,
-                upper_cutoff=upper_cutoff,
-                **kwargs
-            )
+            inversion_method = _exponential_sweep_time_reversal_deconvolution
         else:
             raise ValueError(f'Unknown inverse filtering method {inversion_method}')
-    else:
-        inverse_filter = inversion_method(
-            reference_signal=reference_signal,
-            samplerate=samplerate,
-            lower_cutoff=lower_cutoff,
-            upper_cutoff=upper_cutoff,
-            **kwargs
-        )
 
-    measured_signal = np.asarray(measured_signal)
-    num_samples = measured_signal.shape[-1]
-    impulse_response = np.fft.irfft(
-        np.fft.rfft(measured_signal, n=2 * num_samples, axis=-1)
-        * np.fft.rfft(inverse_filter, n=2 * num_samples),
-        axis=-1
+    impulse_response = inversion_method(
+        reference_signal=reference_signal,
+        measured_signal=measured_signal,
+        samplerate=samplerate,
+        lower_cutoff=lower_cutoff,
+        upper_cutoff=upper_cutoff,
+        **kwargs
     )
+
     if ir_orders == 'whole':
         return impulse_response
 
-    irs = [impulse_response[..., num_samples.size:]]
+    num_samples = impulse_response.shape[-1] // 2
+
+    irs = [impulse_response[..., :num_samples]]
     if ir_orders > 1:
-        phase_rate = inverse_filter.size / np.log(upper_cutoff / lower_cutoff)
+        phase_rate = reference_signal.size / np.log(upper_cutoff / lower_cutoff)
         lags = np.log(np.arange(1, ir_orders + 1)) * phase_rate  # How much each impulse response is before the linear one
         indices = num_samples - np.ceil(lags).astype(int)  # The index where each of the impulse responses start
         for order in range(1, ir_orders):  # Starts with order 1, corresponding to the first harmonic.
@@ -87,14 +60,20 @@ def sweep_deconvolution(
     return irs
 
 
-def _filtered_inverse(
+def _filtered_deconvolution(
     reference_signal,
+    measured_signal,
     samplerate=None,
     lower_cutoff=None,
     upper_cutoff=None,
     **kwargs
 ):
-    inverse_signal = np.fft.irfft(1 / np.fft.rfft(reference_signal))
+    measured_signal = np.asarray(measured_signal)
+    num_samples = measured_signal.shape[-1]
+    impulse_response = np.fft.irfft(
+        np.fft.rfft(measured_signal, 2 * num_samples)
+        / np.fft.rfft(reference_signal, 2 * num_samples)
+    )
 
     filter_args = kwargs or {}
     if lower_cutoff is not None and upper_cutoff is not None:
@@ -107,7 +86,7 @@ def _filtered_inverse(
         filter_args.setdefault('Wn', upper_cutoff)
         filter_args['btype'] = 'lowpass'
     else:
-        return inverse_signal
+        return impulse_response
 
     filter_args.setdefault('N', 8)
     filter_args.setdefault('fs', samplerate)
@@ -115,12 +94,13 @@ def _filtered_inverse(
     filter_args['output'] = 'sos'
 
     sos = scipy.signal.iirfilter(**filter_args)
-    inverse_signal = scipy.signal.sosfilt(sos, inverse_signal)
-    return inverse_signal
+    impulse_response = scipy.signal.sosfilt(sos, impulse_response)
+    return impulse_response
 
 
-def _regularized_inverse(
+def _regularized_deconvolution(
     reference_signal,
+    measured_signal,
     samplerate=None,
     lower_cutoff=None,
     upper_cutoff=None,
@@ -128,11 +108,18 @@ def _regularized_inverse(
     interior_regularization=0.01,
     exterior_regularization=1,
 ):
-    reference_spectrum = np.fft.rfft(reference_signal)
+    measured_signal = np.asarray(measured_signal)
+    num_samples = measured_signal.shape[-1]
+
+    reference_spectrum = np.fft.rfft(reference_signal, 2 * num_samples)
     samplerate = samplerate or 2  # No given samplerate -> frequencies normalized to Nyquist as in iirfilter
+    f = np.fft.rfftfreq(2 * num_samples, 1 / samplerate)
+    if lower_cutoff is None:
+        lower_cutoff = f[1]
+    if upper_cutoff is None:
+        upper_cutoff = f[-1]
     lower_cutoff_interior = lower_cutoff * 2**transition_width
     upper_cutoff_interior = upper_cutoff / 2**transition_width
-    f = np.fft.rfftfreq(reference_signal.size, 1 / samplerate)
 
     regularization = np.zeros(reference_spectrum.shape)
     interior_idx = (lower_cutoff_interior <= f) & (f <= upper_cutoff_interior)
@@ -149,33 +136,61 @@ def _regularized_inverse(
 
     regularization *= np.mean(np.abs(reference_spectrum[interior_idx])**2 / regularization[interior_idx]) * interior_regularization
     inverse_spectrum = reference_spectrum.conj() / (reference_spectrum.conj() * reference_spectrum + regularization)
-    inverse_signal = np.fft.irfft(inverse_spectrum)
-    return inverse_signal
+
+    impulse_response = np.fft.irfft(np.fft.rfft(measured_signal, n=2 * num_samples) * inverse_spectrum)
+    return impulse_response
 
 
-def _exponential_sweep_analytical_inverse(
-    num_samples,
+def _exponential_sweep_analytical_deconvolution(
+    reference_signal,
+    measured_signal,
     samplerate,
     lower_cutoff,
     upper_cutoff,
 ):
+    num_samples = reference_signal.size
     phase_rate = num_samples / samplerate / np.log(upper_cutoff / lower_cutoff)
     f = np.fft.rfftfreq(num_samples, 1 / samplerate)
     with np.errstate(divide='ignore', invalid='ignore'):
         inverse_spectrum = 2 * (f / phase_rate)**0.5 * np.exp(-2j * np.pi * f * phase_rate * (1 - np.log(f / lower_cutoff)) + 1j * np.pi / 4)
     inverse_spectrum[0] = 0
-    inverse_signal = np.fft.irfft(inverse_spectrum) / samplerate
-    return inverse_signal
+    # We have to go back to time to zero pad.
+    # It's much easier to generate the correct spectrum with the original reference length.
+    inverse_filter = np.fft.irfft(inverse_spectrum) / samplerate
+
+    measured_signal = np.asarray(measured_signal)
+    num_samples = measured_signal.shape[-1]
+
+    impulse_response = np.fft.irfft(
+        np.fft.rfft(measured_signal, n=2 * num_samples)
+        * np.fft.rfft(inverse_filter, n=2 * num_samples),
+    )
+    impulse_response = np.roll(impulse_response, -reference_signal.size, axis=-1)
+
+    return impulse_response
 
 
-def _exponential_sweep_time_reversal_inverse(
+def _exponential_sweep_time_reversal_deconvolution(
     reference_signal,
+    measured_signal,
     lower_cutoff,
     upper_cutoff,
+    samplerate=None,
 ):
     amplitude_correction = 10**(np.linspace(0, -6 * np.log2(upper_cutoff / lower_cutoff), reference_signal.size) / 20)
-    inverse_signal = reference_signal[-1::-1] * amplitude_correction
-    return inverse_signal
+    inverse_filter = reference_signal[-1::-1] * amplitude_correction
+
+    measured_signal = np.asarray(measured_signal)
+    num_samples = measured_signal.shape[-1]
+
+    impulse_response = np.fft.irfft(
+        np.fft.rfft(measured_signal, n=2 * num_samples, axis=-1)
+        * np.fft.rfft(inverse_filter, n=2 * num_samples),
+        axis=-1
+    )
+    impulse_response = np.roll(impulse_response, -reference_signal.size, axis=-1)
+
+    return impulse_response
 
 
 def mls_analysis(reference, output):
